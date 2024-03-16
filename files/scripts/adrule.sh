e6644a67d2e8c01e0a2faa661f9e0ae6591b800a
yoier <85206724+yoier@users.noreply.github.com>
2024-03-16 17:31:27 +0800

    Update adrule.sh

--- files/scripts/adrule.sh
+++ files/scripts/adrule.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
-#dnsmasq的广告屏蔽更新脚本
-#国内用户需要passwall开启路由器本机代理
-#by github yoier [https://github.com/yoier/r2s-firmware-build/edit/main/files/scripts/adrule.sh]
+# dnsmasq的广告屏蔽更新脚本
+# 国内用户需要passwall开启路由器本机代理
+# by github yoier [https://github.com/yoier/r2s-firmware-build/edit/main/files/scripts/adrule.sh]
 
 cd /tmp
 wget -q https://raw.githubusercontent.com/neodevpro/neodevhost/master/dnsmasq.conf -O dnsmasq.conf.tmp0
