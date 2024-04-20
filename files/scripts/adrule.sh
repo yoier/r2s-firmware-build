9e546dec1bbe6e11b735fe0d51e37776157bf5e0
Yoier <huaimingdeng@gmail.com>
2024-04-20 17:15:23 +0800

    no

--- files/scripts/adrule.sh
+++ files/scripts/adrule.sh
@@ -14,7 +14,7 @@ wget -q -O - https://easylist-downloads.adblockplus.org/easyprivacy.txt >> dnsma
 egrep '^\|\|([a-zA-Z\.]+)\^$' dnsmasq.conf.tmp1 | cut -d '^' -f1 | sed 's#||#address=/# ; s#$#/0.0.0.0#' >> dnsmasq.conf.tmp0
 sort dnsmasq.conf.tmp0 | uniq > /etc/dnsmasq.conf
 
-
+#50 4 * * * /scripts/adrule.sh
 #mv -f smartdns.conf /etc/smartdns/adrule.conf
 #/etc/init.d/dnsmasq restart
 exit
